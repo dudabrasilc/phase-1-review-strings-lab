d6b1e2ef1cab9fca13c73dc860b3035d7b53b853
Maria Coitinho <brasildu@gmail.com>
2022-06-27 21:32:27 -0500

    commit tree 97d4f2a4f17ba462a60d5111e922172b3e1528b9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+/*const currentUser = 'Maria';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`  
+const restOfName = currentUser.slice(1,5);
+
+
+console.log(excitedWelcomeMessage);
+console.log(welcomeMessage);
+console.log(firstInitial);
+console.log(restOfName);
+console.log(shortGreeting); */
+
+
+const currentUser = 'Maria';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+
+
+console.log(currentUser);
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(firstInitial);
+console.log(shortGreeting);
